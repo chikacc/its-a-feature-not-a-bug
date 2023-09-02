136e5174628cf618b9e394668cbc060c70c28649
Goo <kuwoods@hotmail.com>
2023-09-02 17:22:51 +0800

    Implement card data import

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private TextAsset data;
+
+    public static GameManager Instance;
+
+    public Dictionary<string, string> Cards = new();
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            DestroyImmediate(gameObject);
+            return;
+        }
+
+        string[] lines = data.text.Split("\r\n");
+
+        for(int i = 0; i < lines.Length; i++)
+        {
+            string[] datas = lines[i].Split(',');
+            Cards.Add(datas[0], datas[1]);
+        }
+    }
+}
